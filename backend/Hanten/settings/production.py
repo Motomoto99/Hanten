48d8d9a289d8812ffe961f8a58b38af3923fb904
Motomoto99 <motoshou39@icloud.com>
2025-07-20 23:07:59 +0900

    本番環境接続設定

--- backend/Hanten/settings/production.py
+++ backend/Hanten/settings/production.py
@@ -10,6 +10,11 @@ if RENDER_EXTERNAL_HOSTNAME:
 
 # ======== データベース設定（最重要！）========
 
+# SECRET_KEYを環境変数から読み込む
+SECRET_KEY = os.environ.get('SECRET_KEY')
+if SECRET_KEY is None:
+    raise ImproperlyConfigured("本番環境変数 'SECRET_KEY' が設定されていません。")
+
 # 環境変数 DATABASE_URL を取得
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
@@ -22,8 +27,17 @@ DATABASES = {
     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
-
+ 許可するホスト名（RenderのドメインとVercelのドメイン）
+ALLOWED_HOSTS = [
+    'hanten.onrender.com',      # Renderのバックエンドドメイン
+    'hanten-psi.vercel.app',  # Vercelのフロントエンドドメイン
+]
 
 CORS_ALLOWED_ORIGINS = [
     "https://hanten-psi.vercel.app",   # ← 本番用の、Vercelの住所をここに追加！(あなたのURLに変えてくださいね)
+]
+
+# CSRF設定: Vercelを信頼できるオリジンとして追加
+CSRF_TRUSTED_ORIGINS = [
+    'https://hanten-psi.vercel.app',
 ]

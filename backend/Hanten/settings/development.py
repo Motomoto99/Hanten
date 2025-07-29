8116b3252ed12556e11152b6a2a025b72551b9b2
Motomoto99 <motoshou39@icloud.com>
2025-07-30 01:03:45 +0900

    DjangoのAdmin使えるように修正

--- backend/Hanten/settings/development.py
+++ backend/Hanten/settings/development.py
@@ -19,3 +19,11 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000", # フロントエンド（Next.js）からのアクセスを許可
 ]
+
+# =================================================================
+#  管理者ユーザー自動作成用の設定
+# =================================================================
+# 環境変数からスーパーユーザーの情報を読み込んで、settingsの一部として定義する
+SUPERUSER_NAME = os.environ.get('SUPERUSER_NAME')
+SUPERUSER_EMAIL = os.environ.get('SUPERUSER_EMAIL')
+SUPERUSER_PASSWORD = os.environ.get('SUPERUSER_PASSWORD')
--- backend/api/testdb/admin.py
+++ backend/api/testdb/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Hello  # 自分のモデルをインポート
 
 # Register your models here.
+admin.site.register(Hello) # この一行を追加！

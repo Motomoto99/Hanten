28abae4fbc9283157670f698aefddc7ded7c72e9
Motomoto99 <motoshou39@icloud.com>
2025-08-10 10:54:53 +0900

    名前の変更

--- backend/api/debate/views.py
+++ backend/api/debate/views.py
@@ -11,7 +11,7 @@ class DebateListAPIView(generics.ListAPIView):
     ディベート部屋の一覧を取得するAPI
     """
     queryset = Room.objects.select_related('theme').annotate( 
-        participant_count=Count('participants') # 参加者数をカウントするためのアノテーション
+        participant_count=Count('participate') # 参加者数をカウントするためのアノテーション
     ).order_by('-room_start') # 最新のディベート部屋が先頭に来るように並び替え
     serializer_class = RoomListSerializer
     # permission_classes = [ClerkAuthenticated] # ログインユーザーのみアクセス可

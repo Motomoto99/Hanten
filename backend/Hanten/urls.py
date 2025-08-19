e7ea4eb8ad26777a54c449efbc570a43101b44d2
Motomoto99 <motoshou39@icloud.com>
2025-08-19 21:52:36 +0900

    接続確認の関数を追加

--- backend/Hanten/urls.py
+++ backend/Hanten/urls.py
@@ -19,6 +19,29 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.http import JsonResponse
+import redis
+
+def health_check(request):
+    redis_url = os.environ.get("REDIS_URL")
+    redis_status = "Not Configured"
+    redis_connection_ok = False
+    if redis_url:
+        try:
+            # 実際にRedisに接続を試みる！
+            r = redis.from_url(redis_url)
+            r.ping()
+            redis_status = "OK"
+            redis_connection_ok = True
+        except Exception as e:
+            redis_status = f"Failed: {str(e)}"
+    
+    return JsonResponse({
+        "django_status": "OK",
+        "redis_status": redis_status,
+        "redis_connection_ok": redis_connection_ok,
+    })
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/testapi/', include('api.testapi.urls')),

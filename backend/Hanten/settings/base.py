5c43b16bbd41e6a23a1c1e3eb284d9dfb29b8568
Motomoto99 <motoshou39@icloud.com>
2025-08-19 19:05:49 +0900

    空白削除かな

--- backend/Hanten/settings/base.py
+++ backend/Hanten/settings/base.py
@@ -141,7 +141,7 @@ if 'REDIS_URL' in os.environ:
         "default": {
             "BACKEND": "channels_redis.core.RedisChannelLayer",
             "CONFIG": {
-                "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+                "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')], 
             },
         },
     }
